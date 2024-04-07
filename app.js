1401317ac775e021a8201399f13e1057f858829b
matheshme <133367307+matheshme@users.noreply.github.com>
2024-04-07 11:28:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,37 +3,20 @@ var port = process.env.PORT || 3000,
     fs = require('fs'),
     html = fs.readFileSync('index.html');
 
-var log = function(entry) {
-    fs.appendFileSync('/tmp/sample-app.log', new Date().toISOString() + ' - ' + entry + '\n');
-};
-
 var server = http.createServer(function (req, res) {
-    if (req.method === 'POST') {
+    if (req.method === 'POST' && req.url === '/') {
         var body = '';
-
         req.on('data', function(chunk) {
             body += chunk;
         });
-
         req.on('end', function() {
-            if (req.url === '/') {
-                log('Received message: ' + body);
-            } else if (req.url = '/scheduled') {
-                log('Received task ' + req.headers['x-aws-sqsd-taskname'] + ' scheduled at ' + req.headers['x-aws-sqsd-scheduled-at']);
-            }
-
-            res.writeHead(200, 'OK', {'Content-Type': 'text/plain'});
-            res.end();
+            console.log('Received message: ' + body);
         });
-    } else {
-        res.writeHead(200);
-        res.write(html);
-        res.end();
     }
+    res.writeHead(200);
+    res.write(html);
+    res.end();
 });
 
-// Listen on port 3000, IP defaults to 127.0.0.1
 server.listen(port);
-
-// Put a friendly message on the terminal
 console.log('Server running at http://127.0.0.1:' + port + '/');
